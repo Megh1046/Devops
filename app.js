91df277b2580374cf3238cc8b1be67cb506092aa
Megh1046 <meghdiwan1046@gmail.com>
2026-07-11 13:58:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let app = express();
+let port = 9000;
+let cors = require('cors');
+
+app.use(cors());
+
+app.get('/ping', (req, res) => {
+    res.send({
+        message: 'hello, world!!!'
+    });
+
+});
+
+app.listen(port,() =>{
+    console.log(`server is running on http://localhost:${port}`);
+});
